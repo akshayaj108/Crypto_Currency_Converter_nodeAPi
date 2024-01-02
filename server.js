f76f5e02e89f7d9d84550d796b21b2ab0e9bdf5a
akshayaj108 <akshayjadhao806@gmail.com>
2024-01-03 01:34:04 +0530

    minor changes

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ import router from "./routes/route.js";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use("/fetch", router);
 app.get("/", (req, res) => {
